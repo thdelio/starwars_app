6a92925b1dd9a9421e9ca9867ae1c5b17f64ee01
sander0050 <sander0050@hotmail.com>
2024-04-14 17:29:15 -0400

    cambios en configuraciones 9

--- jenkins/scripts/killport.bat
+++ jenkins/scripts/killport.bat
@@ -4,4 +4,9 @@ for /f "tokens=5" %%a in ('netstat -aon ^| find ":3000"') do (
     echo Proceso encontrado: %%a
     taskkill /F /PID %%a
 )
+
+if not exist %%a (
+    echo No se encontraron procesos utilizando el puerto 3000.
+)
+
 echo Puerto 3000 liberado exitosamente.
